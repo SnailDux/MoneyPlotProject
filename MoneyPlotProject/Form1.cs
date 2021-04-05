a63561de5d273c1ebdf950c3915f8116911c609a
Przemysław Kudełka <235336@student.pwr.edu.pl>
2021-04-05 11:56:11 +0200

    Fixed naming in chart, now works properly

--- MoneyPlotProject/Form1.cs
+++ MoneyPlotProject/Form1.cs
@@ -31,18 +31,18 @@ namespace MoneyPlotProject
         {
             var p = new MoneyPlotProject.LoadDataHelper();
 
-            chart1.Series["PLN/USD"].Color = Color.Red;
-            chart1.Series["GBP/USD"].Color = Color.Blue;
-            chart1.Series["EUR/USD"].Color = Color.Green;
+            chart1.Series["USD/PLN"].Color = Color.Red;
+            chart1.Series["USD/GBP"].Color = Color.Blue;
+            chart1.Series["USD/EUR"].Color = Color.Green;
 
             int year = int.Parse(textBox1.Text);
             int n = 2021 - year + 1;
             for (int i = 0; i < n; i++)
             {
                 Money m = await p.Load(i+year);
-                chart1.Series["PLN/USD"].Points.AddXY(i+year, m.Rates.PLN);
-                chart1.Series["GBP/USD"].Points.AddXY(i+year, m.Rates.GBP);
-                chart1.Series["EUR/USD"].Points.AddXY(i+year, m.Rates.EUR);
+                chart1.Series["USD/PLN"].Points.AddXY(i+year, m.Rates.PLN);
+                chart1.Series["USD/GBP"].Points.AddXY(i+year, m.Rates.GBP);
+                chart1.Series["USD/EUR"].Points.AddXY(i+year, m.Rates.EUR);
 
             }
         }
